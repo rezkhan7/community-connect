fffdb5c19f975b8afa66a1c9b7033dd65651a700
rezkhan7 <73037837+rezkhan7@users.noreply.github.com>
2024-06-11 18:42:38 -0400

    Added grid from material ui to style the form, and added requirments for username

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useForm, SubmitHandler } from 'react-hook-form'
-import { Button, TextField } from '@mui/material'
+import { Button, TextField, Grid } from '@mui/material'
 
 interface IFormInput {
     firstName: string,
@@ -9,7 +9,7 @@ interface IFormInput {
 }
 
 export default function Signup() {
-    const { register, errors, handleSubmit } = useForm<IFormInput>();
+    const { register, formState: {errors}, handleSubmit } = useForm<IFormInput>();
 
     const onSubmit: SubmitHandler<IFormInput> = (data)=>{
         console.log('Success')
@@ -17,13 +17,35 @@ export default function Signup() {
     }
     console.log("Errors:", errors)
     return(
-        <form onSubmit = {handleSubmit(onSubmit)}>
-          <TextField 
-            id = 'username' 
-            label = 'Username'
-            {...register('username', {required: true})} 
-          />
-          <Button variant='contained' type='submit'> Sign up</Button>  
+        <form onSubmit = {handleSubmit(onSubmit)} autoComplete='off'>
+          <Grid
+            container
+            direction = 'column'
+            alignItems = 'center'
+            justifyContent = 'center'
+            spacing = {2}
+          >
+            <Grid item>
+                <TextField 
+                    id = 'username' 
+                    label = 'Username'
+                    {...register('username', {
+                        required: {value: true, message: 'Please enter a username.'},
+                        minLength: {
+                            value: 3,
+                            message: 'Please enter a username between 3-16 characters'
+                        },
+                        maxLength: {
+                            value: 16,
+                            message: 'Please enter a username between 3-16 characters'
+                        }
+                    })} 
+                />
+            </Grid>
+            <Grid>
+                <Button variant='contained' type='submit'> Sign up</Button>
+            </Grid>
+          </Grid> 
         </form>
     )
 }
